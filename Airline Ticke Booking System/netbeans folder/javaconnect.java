cdc664784d59ce8f0354d33c88b0822cdba62dfe
Sakshi <Sakshi@Sakshee>
2023-03-09 18:06:54 +0530

    Project uploaded Successfully

---
+++ Airline Ticke Booking System/netbeans folder/javaconnect.java
@@ -0,0 +1,35 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author saqib khan
+ */
+public class javaconnect {
+    Connection conn;
+    public static Connection ConnectDb(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/atbs","root","");
+            System.out.println("Database Connected");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+            
+            
+        }
+        
+        
+        
+    }
+    
+}
